53f5d61637a55d737af3b4b509707cde26dc335f
fhtgrjlirtjgv <maksikdonlove001@gmail.com>
2024-09-26 20:30:09 +0300

    43ggfduy5

--- main.py
+++ main.py
@@ -2,26 +2,42 @@ from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRectangleFlatButton
 from kivy.lang import Builder
+import requests
+from settings import *
+
 
 class WScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
-class LCWApp(MDApp):
+    def get_weather(self, city):
+        params = {
+            "q": city,
+            "appid": API_KEY,
+        }
+        data = requests.get(CURRENT_WEATHER_URL, params)
+        respons = data.json()
+        print(respons)
+        return respons
+
+    def search(self):
+        city = self.ids.city.text
+        weather = self.get_weather(city)
+
+        temp = weather["main"]["temp"]
+        self.ids.temp.text = f"{round(temp)}°C"
+        
+
+
+
+class LCW(MDApp):
     def build(self):
         Builder.load_file('Been.kv')
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Green"
 
-        return WScreen() (
-            MDScreen(
-                MDRectangleFlatButton(
-                    text="Hello, World",
-                    pos_hint={"center_x": 0.5, "center_y": 0.5},
-                )
-            )
-        )
+        return WScreen()
+    
 
-
-LCWApp().run()
+LCW ().run()
